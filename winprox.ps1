21b4c35049245faec782511885f4dd3c63d6ad23
Khai Doan <khai@dagknows.com>
2024-11-19 16:23:03 -0800

    Inprogress work for Dell Poc

--- winprox.ps1
+++ winprox.ps1
@@ -432,92 +432,114 @@ $proxy_block = {
 
                 # Keep listening for messages in a loop
                 Write-Host "Listening for incoming messages..."
-                while ($websocket.State -eq [System.Net.WebSockets.WebSocketState]::Open) {
-                    $receiveBuffer = New-Object -TypeName byte[] -ArgumentList 4096
-                    $receivedData = [System.IO.MemoryStream]::new()
-
-                    do {
-                        $receiveSegment = [System.ArraySegment[byte]]::new($receiveBuffer)
-                        $receiveTask = $websocket.ReceiveAsync($receiveSegment, [Threading.CancellationToken]::None)
-                        $receiveTask.Wait()
-
-                        $result = $receiveTask.Result
-
-                        if ($result.MessageType -eq [System.Net.WebSockets.WebSocketMessageType]::Close) {
-                            Write-Host "Server initiated close. Closing the connection."
-                            $websocket.CloseAsync([System.Net.WebSockets.WebSocketCloseStatus]::NormalClosure, "Closing", [Threading.CancellationToken]::None).Wait()
-                            break
+                while ($true) {
+                    if ($websocket.State -ne [System.Net.WebSockets.WebSocketState]::Open) {
+                        Write-Host "Detected connection closed.  Trying to re-establish the websocket connection again."
+                        Start-Sleep -Seconds 2
+                        $websocket.ConnectAsync($uri, [Threading.CancellationToken]::None).Wait()
+                    }
+                    try {
+                        $receiveBuffer = New-Object -TypeName byte[] -ArgumentList 4096
+                        $receivedData = [System.IO.MemoryStream]::new()
+    
+                        do {
+                            $receiveSegment = [System.ArraySegment[byte]]::new($receiveBuffer)
+                            $receiveTask = $websocket.ReceiveAsync($receiveSegment, [Threading.CancellationToken]::None)
+                            $receiveTask.Wait()
+    
+                            $result = $receiveTask.Result
+    
+                            if ($result.MessageType -eq [System.Net.WebSockets.WebSocketMessageType]::Close) {
+                                Write-Host "Server initiated close. Closing the connection."
+                                $websocket.CloseAsync([System.Net.WebSockets.WebSocketCloseStatus]::NormalClosure, "Closing", [Threading.CancellationToken]::None).Wait()
+                                break
+                            }
+    
+                            $receivedData.Write($receiveBuffer, 0, $result.Count)
+                        } while (-not $result.EndOfMessage)
+    
+                        if ($websocket.State -ne [System.Net.WebSockets.WebSocketState]::Open) {
+                            Write-Host "Connection closed.  CHECK A.  Continue."
                         }
-
-                        $receivedData.Write($receiveBuffer, 0, $result.Count)
-                    } while (-not $result.EndOfMessage)
-
-                    if ($receivedData.Length -gt 0) {
-                        $receivedBytes = $receivedData.ToArray()
-                        $receivedMessage = [System.Text.Encoding]::UTF8.GetString($receivedBytes)
-                        $receivedJson = $receivedMessage | ConvertFrom-Json
-                        $receivedJsonPretty = $receivedJson | ConvertTo-Json -Depth 4
-                        # Write-Host "Message received: $receivedJsonPretty"
-                        $debug_file = Join-Path -Path $working_directory -ChildPath "debug.txt" 
-                        "" > $debug_file
-                        #$receivedJsonPretty > $debug_file
-                        $user_info = $receivedJson.message.user_info
-                        $conv_id = $receivedJson.message.req.req_obj.conv_id
-                        $iter = $receivedJson.message.req.req_obj.iter
-                        $code_lines = $receivedJson.message.req.req_obj.code
-                        $job_id = $receivedJson.message.req.job_id
-                        $role_token = $receivedJson.token
-                        $runbook_task_id = $receivedJson.message.req.req_obj.runbook_task_id
-                        $starting_child_path = $receivedJson.message.req.req_obj.starting_child_path
-                        $task_id = $receivedJson.message.req.req_obj.task_id
-
-                        $job_folder = Join-Path -Path $working_directory -ChildPath "jobs" 
-                        Ensure-DirectoryExists -directoryPath $job_folder
-
-                        $fileName = "\jobs\$job_id.ps1"
-                        if ($job_id -ne $null) {
-                            $fullPath = Join-Path -Path $working_directory -ChildPath $fileName 
-                            $current_job_file = Join-Path -Path $working_directory -ChildPath "jobs/current_job.txt" 
-                            $streamWriter = [System.IO.StreamWriter]::new($fullPath, $false, [System.Text.Encoding]::UTF8)
-                            $final_execs_url = $execs_url 
-                            $other_info = '$conv_id = "' + $conv_id + '"' + "`n" +
-                                        '$iter = "' + $iter + '"' + "`n" +
-                                        '$token = "' + $role_token + '"' + "`n" +
-                                        '$execs_url = "' + $final_execs_url + '"' + "`n" +
-                                        '$global:dagknows_url = "' + $dagknows_url + '"' + "`n" + 
-                                        '$user_info = ' + "'" + ($user_info | ConvertTo-Json -Compress) + "'" + "`n" + 
-                                        '$user_info = ' + 'ConvertTo-Hashtable($user_info | ConvertFrom-Json)' + "`n"
-                            
-                            foreach ($line in $code_lines) {
-                                if ($line.Contains("DK_OTHER_INFO")) {
-                                    $streamWriter.WriteLine($other_info)
-                                } else {
-                                    $streamWriter.WriteLine($line)
+    
+    
+                        if ($receivedData.Length -gt 0) {
+                            $receivedBytes = $receivedData.ToArray()
+                            $receivedMessage = [System.Text.Encoding]::UTF8.GetString($receivedBytes)
+                            $receivedJson = $receivedMessage | ConvertFrom-Json
+                            $receivedJsonPretty = $receivedJson | ConvertTo-Json -Depth 4
+                            # Write-Host "Message received: $receivedJsonPretty"
+                            $debug_file = Join-Path -Path $working_directory -ChildPath "debug.txt" 
+                            "" > $debug_file
+                            #$receivedJsonPretty > $debug_file
+                            $user_info = $receivedJson.message.user_info
+                            $conv_id = $receivedJson.message.req.req_obj.conv_id
+                            $iter = $receivedJson.message.req.req_obj.iter
+                            $code_lines = $receivedJson.message.req.req_obj.code
+                            $job_id = $receivedJson.message.req.job_id
+                            $role_token = $receivedJson.token
+                            $runbook_task_id = $receivedJson.message.req.req_obj.runbook_task_id
+                            $starting_child_path = $receivedJson.message.req.req_obj.starting_child_path
+                            $task_id = $receivedJson.message.req.req_obj.task_id
+    
+                            $job_folder = Join-Path -Path $working_directory -ChildPath "jobs" 
+                            Ensure-DirectoryExists -directoryPath $job_folder
+    
+                            $fileName = "\jobs\$job_id.ps1"
+                            if ($job_id -ne $null) {
+                                $fullPath = Join-Path -Path $working_directory -ChildPath $fileName 
+                                $current_job_file = Join-Path -Path $working_directory -ChildPath "jobs/current_job.txt" 
+                                $streamWriter = [System.IO.StreamWriter]::new($fullPath, $false, [System.Text.Encoding]::UTF8)
+                                $final_execs_url = $execs_url 
+                                $other_info = '$conv_id = "' + $conv_id + '"' + "`n" +
+                                            '$iter = "' + $iter + '"' + "`n" +
+                                            '$token = "' + $role_token + '"' + "`n" +
+                                            '$execs_url = "' + $final_execs_url + '"' + "`n" +
+                                            '$global:dagknows_url = "' + $dagknows_url + '"' + "`n" + 
+                                            '$user_info = ' + "'" + ($user_info | ConvertTo-Json -Compress) + "'" + "`n" + 
+                                            '$user_info = ' + 'ConvertTo-Hashtable($user_info | ConvertFrom-Json)' + "`n"
+                                
+                                foreach ($line in $code_lines) {
+                                    if ($line.Contains("DK_OTHER_INFO")) {
+                                        $streamWriter.WriteLine($other_info)
+                                    } else {
+                                        $streamWriter.WriteLine($line)
+                                    }
                                 }
+    
+                                $streamWriter.Close()
+                                Write-Host "Full path:" $fullPath
+                                
+                                if (-not $global:modal_box_visible) {
+                                    Set-Content -Path $current_job_file -Value " "
+                                    $global:dialog_job = Start-Job -ScriptBlock $dialogScript -ArgumentList $proxy_domain, $runbook_task_id, $token, $current_job_file, $job_id, $user_info, $dagknows_url, $exit_file, $debug_file
+                                    #$dialogScript.Invoke($proxy_domain, $runbook_task_id, $token, $current_job_file, $job_id, $user_info, $dagknows_url, $exit_file, $debug_file)
+                                    $global:modal_box_visible = $true
+                                }
+    
+                                #$fullPath >> $debug_file
+    
+                                #Write-Host "BEFORE RUNNING THE PROGRAM"
+                                & $fullPath  
+                                #$task_job = Start-Job -ScriptBlock { & $using:fullPath }
+                                #Wait-Job -Job $task_job
+                                #Write-Host "AFTER RUNNING THE PROGRAM"
+                                #$output = Receive-Job -Job $task_job
+                                #Write-Host $output
+                                #Write-Host "Debug file:" $debug_file
+                                #"``" >> $debug_file
+                                #$output >> $debug_file
+
+                                #Remove-Job -Job $task_job
+    
+                                #$websocket.Dispose()  
+                                #break
                             }
-
-                            $streamWriter.Close()
-                            Write-Host "Full path:" $fullPath
-                            
-                            if (-not $global:modal_box_visible) {
-                                Set-Content -Path $current_job_file -Value " "
-                                $global:dialog_job = Start-Job -ScriptBlock $dialogScript -ArgumentList $proxy_domain, $runbook_task_id, $token, $current_job_file, $job_id, $user_info, $dagknows_url, $exit_file, $debug_file
-                                #$dialogScript.Invoke($proxy_domain, $runbook_task_id, $token, $current_job_file, $job_id, $user_info, $dagknows_url, $exit_file, $debug_file)
-                                $global:modal_box_visible = $true
-                            }
-
-                            #$fullPath >> $debug_file
-
-                            #Write-Host "BEFORE RUNNING THE PROGRAM"
-                            #& $fullPath  
-                            $task_job = Start-Job -ScriptBlock { & $using:fullPath }
-                            Wait-Job -Job $task_job
-                            #Write-Host "AFTER RUNNING THE PROGRAM"
-
-                            #$websocket.Dispose()  
-                            break
+                            $receivedData.SetLength(0) # Clear the MemoryStream for the next message
                         }
-                        $receivedData.SetLength(0) # Clear the MemoryStream for the next message
+
+                    } catch {
+                        Write-Host "An unexpected error occurred: $_"
                     }
                 }
 
